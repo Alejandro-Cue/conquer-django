bdc12a65461cf466fc22b6e659918b28d613e320
Alejandro Cué <alejcue@gmail.com>
2026-03-24 09:39:26 +0100

    Detalles de la página y database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        verbose_name='Título',
+        max_length=200
+    )
+    content = models.TextField(
+        verbose_name='Contenido'
+    )
+    author = models.CharField(
+        verbose_name='Autor',
+        max_length=100
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Creado el: ',
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return self.title
